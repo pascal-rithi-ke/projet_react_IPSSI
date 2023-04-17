839ecec4f279ada21c43311b2f5012223c7bc2b2
bflorestal <yayou7710@hotmail.fr>
2023-04-17 16:06:41 +0200

    Routes et controller CRUD ingrédients

--- backend/routes/ingredient.route.js
+++ backend/routes/ingredient.route.js
@@ -1,11 +1,21 @@
 import { Router } from "express";
 
 // Import des fonctions du controller
-import { getIngredients } from "../controllers/ingredient.controller.js";
+import {
+  getAllIngredients,
+  getIngredientById,
+  addIngredient,
+  updateIngredient,
+  deleteIngredient,
+} from "../controllers/ingredient.controller.js";
 
 const router = Router();
 
 // Route pour la récupération des ingrédients
-router.get("/", getIngredients);
+router.get("/", getAllIngredients);
+router.get("/:id", getIngredientById);
+router.post("/", addIngredient);
+router.put("/:id", updateIngredient);
+router.delete("/:id", deleteIngredient);
 
 export { router as ingredientRouter };
