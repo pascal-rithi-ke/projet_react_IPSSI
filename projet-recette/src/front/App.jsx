506e2ba583a8b84282d455fab7bac91f5be6ddb1
bflorestal <yayou7710@hotmail.fr>
2023-04-17 09:46:28 +0200

    fix: No routes matched location '/'

--- projet-recette/src/front/App.jsx
+++ projet-recette/src/front/App.jsx
@@ -1,25 +1,25 @@
-import {Link, Route, Routes} from 'react-router-dom'
-import './style/App.css'
+import { Link, Route, Routes } from "react-router-dom";
+import "./style/App.css";
 
-import Login from './componant/user/Login'
-import Inscription from './componant/user/Inscription'
+import Login from "./componant/user/Login";
+import Inscription from "./componant/user/Inscription";
 
 function App() {
   return (
     <div className="App">
-      <Link to="accueil">Accueil</Link>
+      <Link to="/">Accueil</Link>
       <Link to="/recette">Recette</Link>
       <Link to="/login">Login</Link>
 
       <Routes>
-        <Route path="accueil" element={<h1>Accueil</h1>}/>
-        <Route path="recette" element={<h1>Recette</h1>}/>
+        <Route path="/" element={<h1>Accueil</h1>} />
+        <Route path="recette" element={<h1>Recette</h1>} />
 
-        <Route path="login" element={<Login/>}/>
-        <Route path='inscription' element={<Inscription/>}/>
+        <Route path="login" element={<Login />} />
+        <Route path="inscription" element={<Inscription />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
