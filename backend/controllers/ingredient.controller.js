970be63bd10f4bf6cc5b3f1d69409bc3bbfd8fc3
bflorestal <yayou7710@hotmail.fr>
2023-04-18 21:20:41 +0200

    Update CRUD ingrédients

--- backend/controllers/ingredient.controller.js
+++ backend/controllers/ingredient.controller.js
@@ -30,48 +30,81 @@ export const getIngredientById = (req, res) => {
 };
 
 export const addIngredient = (req, res) => {
-  const { name, unit } = req.body;
+  const { name, unitId } = req.body;
 
-  let ingredientResult;
-
+  // Ajout de l'ingrédient dans la table ingredient s'il n'existe pas déjà dans la base de données
   db_connect.query(
-    "INSERT INTO ingredient (nom) VALUES (?)",
+    "INSERT IGNORE INTO ingredient SET nom = ?",
     [name],
     (err, result) => {
       if (err) {
         console.error(err);
         res.status(500).send("Erreur lors de l'ajout de l'ingrédient");
-      } else {
-        ingredientResult = result;
-
-        db_connect.query(
-          "INSERT INTO liste_ingredient (id_ingredient, id_unite) VALUES (?, ?)",
-          [ingredientResult.insertId, unit],
-          (err, result) => {
-            if (err) {
-              console.error(err);
-              res.status(500).send("Erreur lors de l'ajout de l'ingrédient");
-            }
-            res.send({ ingredientResult, result });
-          }
-        );
       }
+
+      // Récupération de l'id de l'ingrédient ajouté
+      const ingredientId = result.insertId;
+
+      // Ajout de l'ingrédient dans la table liste_ingredient
+      db_connect.query(
+        "INSERT INTO liste_ingredient (id_ingredient, id_unite) VALUES (?, ?)",
+        [ingredientId, unitId],
+        (err, _result) => {
+          if (err) {
+            console.error(err);
+            res.status(500).send("Erreur lors de l'ajout de l'ingrédient");
+          }
+
+          // Renvoi de la réponse au front avec les données de l'ingrédient ajouté
+          res.json({
+            success: true,
+            ingredient: {
+              id: ingredientId,
+              ...req.body,
+            },
+          });
+        }
+      );
     }
   );
 };
 
 export const updateIngredient = (req, res) => {
   const { id } = req.params;
-  const { name, quantity, unit } = req.body;
+  const { name, quantity, unitId } = req.body;
+
+  // Modification de l'ingrédient dans la table ingredient
   db_connect.query(
-    "UPDATE ingredient SET name = ?, quantity = ?, unit = ? WHERE id = ?",
-    [name, quantity, unit, id],
-    (err, result) => {
+    "UPDATE ingredient SET nom = ? WHERE id = ?",
+    [name, id],
+    (err, _result) => {
       if (err) {
         console.error(err);
         res.status(500).send("Erreur lors de la modification de l'ingrédient");
       }
-      res.send(result);
+
+      // Modification de l'ingrédient dans la table liste_ingredient
+      db_connect.query(
+        "UPDATE liste_ingredient SET quantite_ingredient = ?, id_unite = ? WHERE id_ingredient = ?",
+        [quantity, unitId, id],
+        (err, _result) => {
+          if (err) {
+            console.error(err);
+            res
+              .status(500)
+              .send("Erreur lors de la modification de l'ingrédient");
+          }
+
+          // Renvoi de la réponse au front avec les données modifiées de l'ingrédient
+          res.json({
+            success: true,
+            ingredient: {
+              id,
+              ...req.body,
+            },
+          });
+        }
+      );
     }
   );
 };
@@ -86,7 +119,10 @@ export const deleteIngredient = (req, res) => {
         console.error(err);
         res.status(500).send("Erreur lors de la suppression de l'ingrédient");
       }
-      res.send(result);
+      res.json({
+        success: true,
+        message: "L'ingrédient a bien été supprimé",
+      });
     }
   );
 };
